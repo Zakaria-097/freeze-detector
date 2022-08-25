7fd4d24b9ded92d9ef1bc6704a5d1b05f6efe95d
= <=>
2022-08-25 14:26:46 +0100

    find dups without scanning whole video

--- freeze_detector.py
+++ freeze_detector.py
@@ -36,7 +36,6 @@ def start():
     #initial variables
     count = 0
     counter_id = 0
-    goneThroughScan = 0
 
     #remove items in frames directory if stuff already in there
     folder = 'frames'
@@ -55,13 +54,28 @@ def start():
     #clear previous entries
     with open('hashes.txt', 'r+') as tes, \
         open('results.txt', 'r+') as tes2, \
-        open('timestamped_frames.txt', 'r+') as tes3: tes.truncate(0), tes2.truncate(0), tes3.truncate(0)
+        open('timestamped_frames.txt', 'r+') as tes3: tes.truncate(0), tes3.write("initialise"), tes2.truncate(0), tes3.truncate(0)
         
 
     selected_fileName = filedialog.askopenfilename( filetypes = ( ("Video Files", "*.mp4"), ("All Files", "*.*") ))
 
     # Read the video from specified path 
     cap = cv2.VideoCapture(selected_fileName) 
+
+    # hash count
+    hashCount = 1
+
+    # list of the times in video duplicate hash was found
+    timeOfDup= []
+
+    def dupsFound(file, hash, currentTime):
+
+        file.write("\n""   Hash:  " + hash + "      starting time: " + str(currentTime))
+        file.close()
+
+        #after result is written to results.txt -> open them so the user can see the result 
+        webbrowser.open("timestamped_frames.txt")
+        webbrowser.open("results.txt")
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -81,7 +95,7 @@ def start():
 
             # writing the extracted images
             cv2.imwrite(frame_name, frame)
-
+             
             #hashing each .openframe
             zaks_Hash = imagehash.phash(Image.open(frame_name))
 
@@ -99,13 +113,30 @@ def start():
             file3.write("\n" + current_time_of_video + " " +  string_of_frame_name + " = " + str(zaks_Hash))
             file3.close()
 
-            #iterate over each 3 seconds in the video / used for timestamped_frames.txt
+            # iterate over each 3 seconds in the video / used for timestamped_frames.txt
             counter_id = counter_id + 3
-
-            goneThroughScan = 1
 
             cap.set(1, count)
 
+            timeOfDup.append(str(current_time_of_video))
+
+            # compare current hash to previous hash
+            with open('hashes.txt') as infile:
+
+                lines = infile.read().splitlines()
+                currentHash = lines[-1]
+                previousHash = lines[-2]
+
+                if currentHash == previousHash:
+                    hashCount +=1
+                else:
+                    timeOfDup[:] = []
+
+            resFile = open("results.txt", "a")
+
+            if hashCount == 4:
+                dupsFound(resFile, str(zaks_Hash), timeOfDup)
+ 
             #break while scanning if user presses 'q'
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -113,51 +144,19 @@ def start():
         else: 
             cap.release()
             break
-        
-    def openResults():
-        #after result is written to results.txt -> open them so the user can see the result 
-        webbrowser.open("timestamped_frames.txt")
-        webbrowser.open("results.txt")
 
-    def noDupsFound():
+    # Release all space and windows once done 
+    cv2.destroyAllWindows()
+
+    if hashCount == 1:
+
         #write to results txt file that no dups found
         file20 = open("results.txt","a")
         file20.write( "\n" + "   Scanning Complete:"  + "\n" + "\n" + "   No Freeze Found! ")
         file20.close()
-        
+    
         #spit out the result to inform the user
         webbrowser.open("results.txt")
-
-    # unnecessary optimization step - this skips the first 81 lines in the txt file - 81 lines = 4 minutes
-    testing_file = open('hashes.txt')
-    lines = testing_file.readlines()[72:]
-    testing_file.close()
-
-    #determine if there exists 3 consecutive hashes that are the same in hashes.txt
-    with open('hashes.txt') as infile:
-        counts = collections.Counter(l.strip() for l in lines)
-        variable_test = 0
-        
-    print ("\n" + "   Scanning 100% Complete ")  
-
-    for line, counts in counts.most_common():
-
-        file2 = open("results.txt","a")
-
-        if (counts > 3):
-
-            variable_test = 1
-            file2.write("\n""   Hash:  " + line + "           duplicates: " + str(counts))
-            file2.close()
-    
-    if (variable_test == 1 and goneThroughScan == 1): 
-        openResults()
-                        
-    if (variable_test == 0 and goneThroughScan == 1): 
-        noDupsFound()
-        
-    # Release all space and windows once done 
-    cv2.destroyAllWindows()
 
     exit_program()
 
