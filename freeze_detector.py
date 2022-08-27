0d406b7bfe2db42812780cf826ba2ba209794390
Zakaria <zakaria@blokur.com>
2022-08-27 15:08:32 +0100

    adding comments

--- freeze_detector.py
+++ freeze_detector.py
@@ -7,53 +7,29 @@ import imagehash
 import collections 
 import os, shutil
 import webbrowser
-import tkinter
 from tkinter import filedialog #allow users to select video 
 import art 
 from PIL import Image
 
-try:
-    # creating a folder named data 
-    if not os.path.exists('frames'): 
-        os.makedirs('frames')
-    if not os.path.exists('results.txt'):
-        os.mknod('results.txt')
-    if not os.path.exists('hashes.txt'):
-        os.mknod('hashes.txt')
-    if not os.path.exists('timestamped_frames.txt'):
-        os.mknod('timestamped_frames.txt')
-    # if not created then raise error
-except OSError: 
-    print ('Error: Creating directory of data') 
 
-
-def dupsFound(hash, currentTime):
-
-    file = open("results.txt", "a")
-    file.write("\n""   Duplicate Hash:  " + hash + "      Time: " + str(currentTime[0]))
-    file.close()
-
-    webbrowser.open("results.txt")
+# generate directory 'frames' and all necessary files 
+def generateFiles():
     
-def noDupsFound():
+    try:
+        if not os.path.exists('frames'): 
+            os.makedirs('frames')
+        if not os.path.exists('results.txt'):
+            os.mknod('results.txt')
+        if not os.path.exists('hashes.txt'):
+            os.mknod('hashes.txt')
+        if not os.path.exists('timestamped_frames.txt'):
+            os.mknod('timestamped_frames.txt')
+
+    except OSError: 
+        print ('Error: Creating directory of data') 
+      
+def clearPreviousEntries():
     
-    file20 = open("results.txt","a")
-    file20.write( "\n" + "   Scanning Complete:"  + "\n" + "\n" + "   No Freeze Found! ")
-    file20.close()
-
-    webbrowser.open("results.txt")
-
-def start():
-
-    def exit_program():
-        import sys
-        sys.exit()
-
-    #initial variables
-    count = 0
-    counter_id = 0
-
-    #remove items in frames directory if stuff already in there
     folder = 'frames'
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
@@ -65,24 +41,51 @@ def start():
                 shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-
-    #clear previous entries
+            
     with open('hashes.txt', 'r+') as tes, \
         open('results.txt', 'r+') as tes2, \
         open('timestamped_frames.txt', 'r+') as tes3: tes.truncate(0), tes3.write("initialise"), tes2.truncate(0), tes3.truncate(0)
+                
+def exit_program():
+    import sys
+    sys.exit()
         
+def duplicatesFound(hash, currentTime):
 
+    file = open("results.txt", "a")
+    file.write("\n" + "   Freeze found!" + "\n\n""   Duplicate Hash:  " + hash + "      Time: " + str(currentTime[0]))
+    file.close()
+
+    webbrowser.open("results.txt")
+    
+def noDuplicatesFound():
+    
+    file20 = open("results.txt","a")
+    file20.write( "\n" + "   Scanning Complete:"  + "\n" + "\n" + "   No Freeze Found! ")
+    file20.close()
+
+    webbrowser.open("results.txt")
+
+def start():
+
+    # generate necessary files
+    generateFiles()
+    
+    # clear data from previous scan
+    clearPreviousEntries()
+
+    # set initial variables
+    count = 0
+    counter_id = 0
+    hashCount = 1
+    timeOfDup= [] # list the exact times we've found duplicate hashes 
+    
+    # allow user to select a video file
     selected_fileName = filedialog.askopenfilename( filetypes = ( ("Video Files", "*.mp4"), ("All Files", "*.*") ))
 
-    # Read the video from specified path 
+    # read video from specified path 
     cap = cv2.VideoCapture(selected_fileName) 
 
-    # hash count
-    hashCount = 1
-
-    # list of the times in video duplicate hash was found
-    timeOfDup= []
 
 
 
@@ -142,7 +145,7 @@ def start():
                     timeOfDup[:] = []
 
             if hashCount == 4:
-                dupsFound(str(zaks_Hash), timeOfDup)
+                duplicatesFound(str(zaks_Hash), timeOfDup)
  
             #break while scanning if user presses 'q'
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -157,15 +160,15 @@ def start():
 
     if hashCount < 3:
 
-        noDupsFound()
+        noDuplicatesFound()
         
     exit_program()
 
 
 art.tprint("\nFreeze Detector")
 
-print ("      Welcome to 'Freeze Detector' Developed by SmartOdds Ltd")
-print ("\n" + "      This program was developed by Zakaria for the SAT's Team")
+print ("      Welcome to 'Freeze Detector' Developed Zakaria-097")
+print ("\n" + "      This program is a simple scanner to detect freezes in your video files.")
 
 def askUser():
 
